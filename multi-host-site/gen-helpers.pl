77874773d297cb0c699b9a83813eda777766ca71
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-04-18 20:22:30 +0000

    Latemp: Starting to make it a multi-host dir.

--- multi-host-site/gen-helpers.pl
+++ multi-host-site/gen-helpers.pl
@@ -7,66 +7,74 @@ use File::Find::Rule;
 use File::Basename;
 
 my $dir = "src";
-my @files = File::Find::Rule->in($dir);
 
-s!^src/!! for @files;
-@files = (grep { $_ ne $dir } @files);
-@files = (grep { ! m{(^|/)\.svn(/|$)} } @files);
-@files = (grep { ! /~$/ } @files);
-@files = 
-    (grep 
-    {
-        my $b = basename($_); 
-        !(($b =~ /^\./) && ($b =~ /\.swp$/))
-    } 
-    @files
-    );
-@files = sort { $a cmp $b } @files;
+opendir D, "$dir";
+my @hosts = grep { -d "$_" } grep { !/^\./ } readdir(D);
+closedir(D);
 
-my @buckets = 
-(
-    {
-        'name' => "IMAGES_PRE1",
-        'filter' => sub { (!/\.wml$/) && (-f "$dir/$_") },
-    },
-    {
-        'name' => "SUBDIRS_PROTO",
-        'filter' => sub { (-d "$dir/$_") },
-    },
-    {
-        'name' => "HTMLS_PROTO",
-        'filter' => sub { /\.html\.wml$/ },
-        'map' => sub { my $a = shift; $a =~ s{\.wml$}{}; return $a;},
-    },
-);
-
-foreach (@buckets) 
-{ 
-    $_->{'results'}=[]; 
-    if (!exists($_->{'map'}))
-    {
-        $_->{'map'} = sub { return shift;},
-    }
-}
-
-FILE_LOOP: foreach (@files)
+foreach my $host (@hosts)
 {
-    for my $b (@buckets)
-    {
-        if ($b->{'filter'}->())
+    my $dir_path = "$dir/$host";
+    my @files = File::Find::Rule->in($dir_path);
+
+    s!^$dir_path/!! for @files;
+    @files = (grep { $_ ne $dir } @files);
+    @files = (grep { ! m{(^|/)\.svn(/|$)} } @files);
+    @files = (grep { ! /~$/ } @files);
+    @files = 
+        (grep 
         {
-            push @{$b->{'results'}}, $b->{'map'}->($_);
-            next FILE_LOOP;
+            my $b = basename($_); 
+            !(($b =~ /^\./) && ($b =~ /\.swp$/))
+        } 
+        @files
+        );
+    @files = sort { $a cmp $b } @files;
+
+    my @buckets = 
+    (
+        {
+            'name' => "IMAGES_PRE1",
+            'filter' => sub { (!/\.wml$/) && (-f "$dir/$_") },
+        },
+        {
+            'name' => "SUBDIRS_PROTO",
+            'filter' => sub { (-d "$dir/$_") },
+        },
+        {
+            'name' => "HTMLS_PROTO",
+            'filter' => sub { /\.html\.wml$/ },
+            'map' => sub { my $a = shift; $a =~ s{\.wml$}{}; return $a;},
+        },
+    );
+
+    foreach (@buckets) 
+    { 
+        $_->{'results'}=[]; 
+        if (!exists($_->{'map'}))
+        {
+            $_->{'map'} = sub { return shift;},
         }
     }
-    die "Uncategorized file $_!";
-}
 
-open O, ">", "include.mak";
-foreach my $b (@buckets)
-{
-    print O $b->{'name'} . " = " . join(" ", @{$b->{'results'}}) . "\n";
-}
-close(O);
+    FILE_LOOP: foreach (@files)
+    {
+        for my $b (@buckets)
+        {
+            if ($b->{'filter'}->())
+            {
+                push @{$b->{'results'}}, $b->{'map'}->($_);
+                next FILE_LOOP;
+            }
+        }
+        die "Uncategorized file $_!";
+    }
 
+    open O, ">", "include.mak";
+    foreach my $b (@buckets)
+    {
+        print O uc($host) . "_" . $$b->{'name'} . " = " . join(" ", @{$b->{'results'}}) . "\n";
+    }
+    close(O);
+}
 1;
