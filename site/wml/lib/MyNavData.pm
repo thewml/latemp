b786b5b346acfbae533dd9da3960cd4659c424c4
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-04-21 19:10:11 +0000

    Adding the Latemp own site skeleton.

---
+++ site/wml/lib/MyNavData.pm
@@ -0,0 +1,60 @@
+package MyNavData;
+
+my $hosts =
+{
+    'mysite' =>
+    {
+        'base_url' => "http://web-cpan.berlios.de/latemp/",
+    },
+};
+
+my $tree_contents =
+{
+    'host' => "mysite",
+    'text' => "Latemp",
+    'title' => "The Latemp Content Management System",
+    'subs' =>
+    [
+        {
+            'text' => "Home",
+            'url' => "",
+        },
+        {
+            'text' => "About",
+            'url' => "about/",
+        },
+        {
+            'text' => "Download",
+            'url' => "download/",
+            'title' => "How to Download the Software.",
+        },
+        {
+            'text' => "Documentation",
+            'url' => "docs/",
+        },
+        {
+            'text' => "Examples",
+            'url' => "examples/",
+            'title' => "Complete Example Sites",
+        },
+        {
+            'separator' => 1,
+            'skip' => 1,
+        },
+        {
+            'text' => "Links",
+            'url' => "links/",
+        },
+    ],
+};
+
+sub get_params
+{
+    return 
+        (
+            'hosts' => $hosts,
+            'tree_contents' => $tree_contents,
+        );
+}
+
+1;
