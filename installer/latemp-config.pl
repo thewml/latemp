582d749980f0e2b153dbaebeef61d921872400cc
Shlomi Fish <shlomif@shlomifish.org>
2005-04-26 10:58:14 +0000

    Converted the code to include stuff from $HOME/.latemp/lib and. Converted the web-site accordingly.

--- installer/latemp-config.pl
+++ installer/latemp-config.pl
@@ -7,14 +7,22 @@ use Getopt::Long;
 
 my $prefix = "<<<PREFIX>>>";
 
-my $wml_include_path = 0;
+my $opt_wml_include_path = 0;
+my $opt_wml_flags = 0;
 GetOptions(
-    "wml-include-path" => \$wml_include_path
+    "wml-include-path" => \$opt_wml_include_path,
+    "wml-flags" => \$opt_wml_flags,
 );
 
-if ($wml_include_path)
+my $wml_include_path = "$prefix/lib/wml/include/";
+if ($opt_wml_include_path)
 {
-    print "$prefix/lib/wml/include/\n";
+    print "$wml_include_path\n";
+    exit;
+}
+if ($opt_wml_flags)
+{
+    print "-I$wml_include_path -I" . $ENV{'HOME'} . "/.latemp/lib/". "\n";
     exit;
 }
 
