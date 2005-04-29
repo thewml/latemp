127609a70472a6a3351efb2aefe147947c841b74
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-04-29 19:43:24 +0000

    Commited HTML-Latemp-NavLinks-GenHtml.

---
+++ Perl/modules/HTML-Latemp-NavLinks-GenHtml/lib/HTML/Latemp/NavLinks/GenHtml.pm
@@ -0,0 +1,45 @@
+package HTML::Latemp::NavLinks::GenHtml;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+HTML::Latemp::NavLinks::GenHtml - A module to generate the HTML of the 
+navigation links.
+
+=cut
+
+our $VERSION = '0.1.1';
+
+=head1 SYNOPSIS
+
+    package MyNavLinks;
+
+    use base 'HTML::Latemp::NavLinks::GenHtml::ArrowImages';
+    
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif@iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-html-latemp-navlinks-genhtml@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=HTML-Latemp-NavLinks-GenHtml>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2005 Shlomi Fish, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the MIT X11 license.
+
+=cut
+
+1; 
+
