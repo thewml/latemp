2881a067b461935e30522b5319450bdf1bb6f1a9
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-05-07 08:47:48 +0000

    Added the news management and a new news item.

---
+++ site/wml/lib/MyManageNews.pm
@@ -0,0 +1,64 @@
+package MyManageNews;
+
+use base 'Exporter';
+
+our @EXPORT=(qw(get_news_manager));
+
+use strict;
+use warnings;
+
+use HTML::Latemp::News;
+
+my @news_items =
+(
+    (map 
+        { 
+            +{%$_, 
+                'author' => "Shlomi Fish", 
+                'category' => "Latemp", 
+                'text' => "Hello"
+            }
+        }
+        (
+        {
+            'title' => "First stable version of Latemp - 0.2.0",
+            'id' => "0.2.0",
+            'description' => q{Latemp version 0.2.0 was released. This is
+            the first stable version of Latemp.},
+            'date' => "2005-05-07",
+        },
+        )
+    )
+);
+
+sub gen_news_manager
+{
+    return
+        HTML::Latemp::News->new(
+            'news_items' => \@news_items,
+            'title' => "Latemp News",
+            'link' => "http://web-cpan.berlios.de/latemp/",
+            'language' => "en-US",
+            'copyright' => "Copyright by Shlomi Fish, (c) 2005",
+            'webmaster' => "Shlomi Fish <shlomif\@iglu.org.il>",
+            'managing_editor' => "Shlomi Fish <shlomif\@iglu.org.il>",
+            'description' => ("News of Latemp - the CMS for Static HTML " . 
+                "web-sites."),
+        );
+}
+
+# A singleton.
+{
+    my $news_manager;
+
+    sub get_news_manager
+    {
+        if (!defined($news_manager))
+        {
+            $news_manager = gen_news_manager();
+        }
+        return $news_manager;
+    }
+}
+
+1;
