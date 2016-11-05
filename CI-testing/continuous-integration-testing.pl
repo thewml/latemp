7f8365df8cc65d8466adbfc6e6fad4ddaabaad14
Shlomi Fish <shlomif@shlomifish.org>
2016-11-05 17:21:18 +0200

    Start adding Travis-CI.

---
+++ CI-testing/continuous-integration-testing.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+sub do_system
+{
+    my ($args) = @_;
+
+    my $cmd = $args->{cmd};
+    print "Running [@$cmd]";
+    if (system(@$cmd))
+    {
+        die "Running [@$cmd] failed!";
+    }
+}
+
+my $cmd = shift@ARGV;
+
+# do_system({cmd => ["cd black-hole-solitaire/ && mkdir B && cd B && ../c-solver/Tatzer && make && $^X ../c-solver/run-tests.pl"]});
+
+# do_system({cmd => ["cd black-hole-solitaire/Games-Solitaire-BlackHole-Solver/ && dzil test --all"]});
+if ($cmd eq 'install_deps')
+{
+    do_system({cmd => ["cd Perl/modules/HTML-Latemp-GenMakeHelpers/ && (dzil authordeps --missing | cpanm)"]});
+}
