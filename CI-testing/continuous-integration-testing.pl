cef96e327329dea5549567137c28069db24ed472
Shlomi Fish <shlomif@shlomifish.org>
2016-11-11 11:33:18 +0200

    Add dzil smoke to the Travis-CI.

--- CI-testing/continuous-integration-testing.pl
+++ CI-testing/continuous-integration-testing.pl
@@ -21,8 +21,28 @@ my $cmd = shift@ARGV;
 # do_system({cmd => ["cd black-hole-solitaire/ && mkdir B && cd B && ../c-solver/Tatzer && make && $^X ../c-solver/run-tests.pl"]});
 
 # do_system({cmd => ["cd black-hole-solitaire/Games-Solitaire-BlackHole-Solver/ && dzil test --all"]});
+
+my @dzil_dirs =
+(
+    'Perl/modules/HTML-Latemp-GenMakeHelpers/',
+    'Perl/modules/HTML-Latemp-NavLinks-GenHtml',
+);
+
 if ($cmd eq 'install_deps')
 {
-    do_system({cmd => ["cd Perl/modules/HTML-Latemp-GenMakeHelpers/ && (dzil authordeps --missing | cpanm)"]});
-    do_system({cmd => ["cd Perl/modules/HTML-Latemp-NavLinks-GenHtml && (dzil authordeps --missing | cpanm)"]});
+    foreach my $d (@dzil_dirs)
+    {
+        do_system({cmd => ["cd $d && (dzil authordeps --missing | cpanm)"]});
+    }
+}
+elsif ($cmd eq 'test')
+{
+    foreach my $d (@dzil_dirs)
+    {
+        do_system({cmd => ["cd $d && (dzil smoke --release --author)"]});
+    }
+}
+else
+{
+    die "Unknown command '$cmd'!";
 }
