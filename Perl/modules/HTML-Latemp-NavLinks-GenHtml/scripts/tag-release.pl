196895ac0ceb2de43dd0a8b9ee544830a8cc1b1a
Shlomi Fish <shlomif@shlomifish.org>
2020-01-14 14:21:06 +0200

    update tag-release.pl

--- Perl/modules/HTML-Latemp-NavLinks-GenHtml/scripts/tag-release.pl
+++ Perl/modules/HTML-Latemp-NavLinks-GenHtml/scripts/tag-release.pl
@@ -3,25 +3,23 @@
 use strict;
 use warnings;
 
-use IO::All;
+use IO::Al qw/ io /;
 
 my ($version) =
-    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file('lib/HTML/Latemp/NavLinks/GenHtml.pm')->getlines()
-    )
-    ;
+    ( map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
+        io->file('lib/HTML/Latemp/NavLinks/GenHtml.pm')->getlines() );
 
-if (!defined ($version))
+if ( !defined($version) )
 {
     die "Version is undefined!";
 }
 
 my @cmd = (
-    "hg", "tag", "-m",
+    "git", "tag", "-m",
     "Tagging the HTML-Latemp-NavLinks-GenHtml release as $version",
     "Perl/HTML-Latemp-NavLinks-GenHtml/releases/$version",
 );
 
-print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
 exec(@cmd);
 
