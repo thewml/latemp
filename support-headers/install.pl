e7ea233ba214084145ecd8bf0c3f77abc0c13771
Shlomi Fish <shlomif@shlomifish.org>
2022-10-19 05:16:12 +0300

    Rename variable/s to make them more descriptive.

--- support-headers/install.pl
+++ support-headers/install.pl
@@ -10,22 +10,22 @@ use File::Copy     qw/ copy /;
 
 my $inst_dir = $ENV{'HOME'} . "/conf/wml/Latemp/";
 
-my @files;
+my @filenames;
 
 sub wanted
 {
     my $fn = $File::Find::name;
     if ( ( -f $fn ) and ( $fn !~ m#\.svn# ) )
     {
-        push @files, $fn;
+        push @filenames, $fn;
     }
     return;
 }
 find( \&wanted, "lib/", );
 
-foreach my $f (@files)
+foreach my $fn (@filenames)
 {
-    my $dir = dirname($f);
-    mkpath( ["$inst_dir/$dir"] );
-    copy( $f, "$inst_dir/$f" );
+    my $dn = dirname($fn);
+    mkpath( ["$inst_dir/$dn"] );
+    copy( $fn, "$inst_dir/$fn" );
 }
