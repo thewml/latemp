c24e9d58fa8eed061037db72e1b4d9f192e2b5f5
Shlomi Fish <shlomif@shlomifish.org>
2024-03-29 16:19:05 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- Perl/modules/HTML-Latemp-GenMakeHelpers/examples/shlomifish.org-gen-helpers.pl
+++ Perl/modules/HTML-Latemp-GenMakeHelpers/examples/shlomifish.org-gen-helpers.pl
@@ -3,27 +3,28 @@
 use strict;
 use warnings;
 
+use Cwd                          qw( getcwd );
+use Path::Tiny                   qw/ path /;
 use HTML::Latemp::GenMakeHelpers ();
 
-use Cwd (qw(getcwd));
-
-my $generator =
-    HTML::Latemp::GenMakeHelpers->new(
-        'hosts' =>
-        [ map {
-            +{ 'id' => $_, 'source_dir' => $_,
-                'dest_dir' => "\$(ALL_DEST_BASE)/$_-homepage"
+my $generator = HTML::Latemp::GenMakeHelpers->new(
+    'hosts' => [
+        map {
+            +{
+                'id'         => $_,
+                'source_dir' => $_,
+                'dest_dir'   => "\$(ALL_DEST_BASE)/$_-homepage"
             }
-        } (qw(common t2 vipe)) ],
-    );
+        } (qw( common t2 vipe ))
+    ],
+);
 
 $generator->process_all();
 
-use IO::All qw/ io /;
-
-my $text = io("include.mak")->slurp();
+my $mak_fh = path("include.mak");
+my $text   = $mak_fh->slurp_utf8();
 $text =~ s!^(T2_DOCS = .*)humour/fortunes/index.html!$1!m;
-io("include.mak")->print($text);
+$mak_fh->spew_utf8($text);
 
 # This is to in order to generate the t2/humour/fortunes/arcs-list.mak
 # file, which is inclduded by the makefile.
@@ -31,7 +32,7 @@ io("include.mak")->print($text);
     my $orig_dir = getcwd();
 
     chdir("t2/humour/fortunes");
-    system("make", "dist");
+    system( "make", "dist" );
 
     chdir($orig_dir);
 }
